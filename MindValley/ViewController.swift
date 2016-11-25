11d060440f737ee96354fb13ac286ed93ca1602e
Allan <allan.martins@resource.com.br>
2016-11-25 16:17:12 -0200

    Initial Commit

---
+++ MindValley/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MindValley
+//
+//  Created by resource on 11/25/16.
+//  Copyright © 2016 bienemann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
